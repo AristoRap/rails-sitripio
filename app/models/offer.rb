ae8eb3d5e01f009df7cc5b666608e343f908a8e8
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-30 17:31:04 +0100

    added dependent destroy user for offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ApplicationRecord
-  belongs_to :organizer, class_name: 'User', foreign_key: "organizer_id"
+  belongs_to :organizer, class_name: 'User', foreign_key: "organizer_id", dependent: :destroy
 
   validates :description, presence: true
   validates :location, presence: true
