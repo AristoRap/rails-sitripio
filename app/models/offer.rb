ef057b2e74281cf20a663943d3e795dffc9cc272
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-30 16:43:08 +0100

    added validations for offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :fee, presence: true, numericality: true
+  validates :date, presence: true
 end
