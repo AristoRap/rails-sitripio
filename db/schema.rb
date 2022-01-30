c53f98080618f18e151c79b0b426ad9e0a823c5f
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-30 16:14:42 +0100

    added offer model (excl. organizer reference)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_154523) do
+ActiveRecord::Schema.define(version: 2022_01_30_151301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.text "description"
+    t.string "location"
+    t.float "fee"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
