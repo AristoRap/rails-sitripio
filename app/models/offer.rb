1844057cd593772110b92d822d8e7e42e48b54e1
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-30 17:28:17 +0100

    replaced has_many association from users for offers with manually created method in order to work

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,6 @@
 class Offer < ApplicationRecord
-  belongs_to :organizer, class_name: 'User'
+  belongs_to :organizer, class_name: 'User', foreign_key: "organizer_id"
+
   validates :description, presence: true
   validates :location, presence: true
   validates :fee, presence: true, numericality: true
