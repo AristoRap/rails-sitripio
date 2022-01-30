46d2fb2e2aaed293934c6fd1199b7655d2b7662b
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-30 16:23:30 +0100

    added user reference in offer via organizer field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_151301) do
+ActiveRecord::Schema.define(version: 2022_01_30_151630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_01_30_151301) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "organizer_id", null: false
+    t.index ["organizer_id"], name: "index_offers_on_organizer_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_01_30_151301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offers", "users", column: "organizer_id"
 end
