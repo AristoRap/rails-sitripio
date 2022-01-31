fd43163a5b513ead2a8e7d560e1f24d24579ecc0
bassem riachy <bassem.riachy@gmail.com>
2022-01-31 14:42:02 +0100

    added validations for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   has_many :users
   has_one :offer
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true, date: { after_or_equal_to:  :start_date}
+  validates :accepted, presence: true
 end
