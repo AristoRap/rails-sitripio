8af807158f2723dedf301ea451e53d6f817c1037
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-01 17:31:45 +0100

    added title to offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_151630) do
+ActiveRecord::Schema.define(version: 2022_02_01_163107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_151630) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "organizer_id", null: false
+    t.string "title"
     t.index ["organizer_id"], name: "index_offers_on_organizer_id"
   end
 
