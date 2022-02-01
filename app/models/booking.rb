55b92a7822c441737c9ea52f8ea6acfb854be55e
bassem riachy <bassem.riachy@gmail.com>
2022-02-01 19:24:04 +0100

    Changed bookings to has_one :user, removed accepted column along with its validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,7 @@
 class Booking < ApplicationRecord
-  has_many :users, dependent: :destroy
+  has_one :user, dependent: :destroy
   has_one :offer
 
   validates :start_date, presence: true
   validates :end_date, presence: true, date: { after_or_equal_to:  :start_date}
-  validates :accepted, presence: true
 end
---
+++ db/migrate/20220201182223_remove_field_name_from_table_name.rb
@@ -0,0 +1,5 @@
+class RemoveFieldNameFromTableName < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :table_names, :accepted, :boolean
+  end
+end
