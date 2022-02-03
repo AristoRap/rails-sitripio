7f3fbe3c69664850e050df7d44d2d4637335a769
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-03 20:45:43 +0100

    finished creating users and offers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'Deleting offers'
+Offer.delete_all
+puts 'offers deleted'
+
 puts 'Deleting users'
 User.delete_all
 puts 'Users deleted'
 
 puts '----------------------------------------------------------------'
 
-puts 'Creating users'
+puts 'Creating 3 users'
 3.times do |i|
   puts "Creating user #{i + 1}"
   user = User.new(
@@ -26,6 +30,50 @@ puts 'Creating users'
   else
     puts "XXXXXXXXXXXXXX"
     puts "Issue with user #{i + 1}"
+    puts "XXXXXXXXXXXXXX"
+  end
+end
+
+puts 'Creating 3 offers'
+3.times do |i|
+  puts "Creating offer #{i + 1}"
+  offer = Offer.new(
+    {
+      title: 'Awesome surf camp in Baleal, Portugal',
+      description: 'Awesome surf camp',
+      location: 'Portugal',
+      fee: 99.99,
+      date: Date.today + rand(90),
+      organizer: User.all.sample
+    }
+  )
+  if offer.save
+    puts "Offer #{i + 1} saved!"
+  else
+    puts "XXXXXXXXXXXXXX"
+    puts "Issue with offer #{i + 1}"
+    puts "XXXXXXXXXXXXXX"
+  end
+end
+
+puts 'Creating 3 bookings'
+3.times do |i|
+  puts "Creating booking #{i + 1}"
+  offer = Offer.all.sample
+
+  booking = Booking.new(
+    {
+      user: User.all.reject { |user| user == offer.organizer }.sample,
+      offer: offer,
+      start_date: offer.date,
+      end_date: offer.date + rand(5)
+    }
+  )
+  if booking.save
+    puts "Booking #{i + 1} saved!"
+  else
+    puts "XXXXXXXXXXXXXX"
+    puts "Issue with booking #{i + 1}"
     puts "XXXXXXXXXXXXXX"
   end
 end
