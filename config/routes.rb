9a4393d4980ac4a75f4664e98f30449376632157
bassem riachy <bassem.riachy@gmail.com>
2022-02-03 19:48:18 +0100

    Bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:show, :edit, :update, :destroy]
+  # Do we need the show function since we are including all that in the "Overview" ?
 
   devise_for :users
   root to: 'pages#home'
