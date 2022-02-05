da56b04b005117913d22172daf4cf9bf49c01e37
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-05 15:39:35 +0100

    added photos association to offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :organizer, class_name: 'User', foreign_key: "organizer_id", dependent: :destroy
+  has_many_attached :photos
 
   validates :description, presence: true
   validates :location, presence: true
