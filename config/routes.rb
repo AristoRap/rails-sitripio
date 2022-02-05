54af9f5210e2559a6398644484fa76e285081bb0
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-05 16:59:09 +0100

    added overview route to config

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+  get '/my_overview', to: 'offers#overview', as: 'overview'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
